3ee54c62d1b21e2a58cf53e8a46b990cac17861c
Mahmudul Alam <mahmudula2000@gmail.com>
2024-03-05 19:35:54 +0600

    completed scaling

---
+++ 7. scaling.cpp
@@ -0,0 +1,40 @@
+#include <graphics.h>
+#include <bits/stdc++.h>
+
+int gd = DETECT, gm;
+int n, x[100], y[100], i;
+float sfx, sfy;
+using namespace std;
+
+void draw() {
+    for (i = 0; i < n; i++) {
+        line(x[i], y[i], x[(i + 1) % n], y[(i + 1) % n]);
+    }
+}
+
+void scale() {
+    setcolor(RED);
+    draw();
+    for (i = 0; i < n; i++) {
+        x[i] = x[i] * sfx;
+        y[i] = y[i] * sfy;
+    }
+    setcolor(YELLOW);
+    draw();
+}
+
+int main() {
+    int gd = DETECT, gm;
+	initgraph(&gd, &gm, "");
+    cout << "Enter no. of sides in polygon: ";
+    cin >> n;
+    cout << "Enter coordinates x, y for each vertex: ";
+    for (i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+    cout << "Enter scale factors: sfx and sfy : ";
+    cin >> sfx >> sfy;
+    scale();
+    getch();
+    return 0;
+}
