f2782bca307d98f8c9b9d9aa916a7049e97334ad
Mahmudul Alam <mahmudula2000@gmail.com>
2024-03-05 19:14:46 +0600

    changed octet to match with book equation

--- 5. midpoint_circle_drawing.cpp
+++ 5. midpoint_circle_drawing.cpp
@@ -1,27 +1,20 @@
-// C++ program for implementing
-// Mid-Point Circle Drawing Algorithm
 #include<iostream>
 #include<graphics.h>
 using namespace std;
 
-// Implementing Mid-Point Circle Drawing Algorithm
+
 void midPointCircleDraw(int x_centre, int y_centre, int r) {
-    int x = r, y = 0;
+    int x = 0, y = r;
     int P = 1 - r;
-    while (x > y) {
-        y++;
-
-        // Mid-point is inside or on the perimeter
-        if (P <= 0)
-            P = P + 2*y + 1;
-        // Mid-point is outside the perimeter
-        else {
-            x--;
-            P = P + 2*y - 2*x + 1;
+    while (y > x) {
+        x++;
+        if (P <= 0) {
+            P = P + 2*x + 1;
+        } else {
+            y--;
+            P = P + 2*x - 2*y + 1;
         }
-
-        // All the perimeter points have already been printed
-        if (x < y)
+        if (y < x)
             break;
 
         putpixel(x + x_centre, y + y_centre, WHITE);
@@ -35,7 +28,6 @@ void midPointCircleDraw(int x_centre, int y_centre, int r) {
     }
 }
 
-// Driver code
 int main() {
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "");
